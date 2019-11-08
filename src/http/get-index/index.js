e68d9471a8ff56c0d5bdae3f964fa1bbca85f054
Carter Rabasa <carter@rabasa.com>
2019-11-08 13:32:43 -0800

    fixed caching headers

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -30,7 +30,10 @@ let body = `
 
 exports.handler = async function http(req) {
   return {
-    headers: {'content-type': 'text/html; charset=utf8'},
+    headers: {
+  'content-type': 'text/html; charset=utf8',
+  'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+  },
     body
   }
 }
