18a06c3adf1e1b3728380ecfc3c04f4c129c0dea
kj <kristoferjoseph@gmail.com>
2020-05-26 12:38:25 -0700

    Update adds some comments.

--- src/http/get-speakers/index.js
+++ src/http/get-speakers/index.js
@@ -4,19 +4,23 @@ const isXHR = require('@architect/shared/utils/is-xhr')
 
 exports.handler = async function http(req) {
   let speakers = await data.get({ table: 'speakers' })
+  // Collect all the speakers topics into an array with no duplicate topics
   let topics = speakers.reduce((a, r) => [...new Set(a.concat(r.topics))].sort(), [])
   let params = req.queryStringParameters || {}
+  // Parse topics from the comma separated list in the query params
   let selectedTopics = params.topics &&
     params.topics.split(',')
       .map(t => t.trim())
 
+  // If topics were passed as params
   if (selectedTopics) {
+    // Only return speakers that have one of the selected topics
     speakers = speakers.filter(s => {
       let speakerTopics = s.topics || []
       return speakerTopics.some(speakerTopic => selectedTopics.some(selectedTopic => speakerTopic === selectedTopic))
     })
   }
-
+  // If this is an XHR request respond with JSON
   if (isXHR(req)) {
     return {
       headers: {
