3c4cd4a48d2eb1878a4f8424487a4fc5ce7a4c1a
Carter Rabasa <carter.rabasa@gmail.com>
2020-07-15 11:18:53 -0700

    debugging for SlackView

--- public/js/slackview.js
+++ public/js/slackview.js
@@ -2,7 +2,8 @@
 window.onload = function() {
     // Attach builtin widget to your page
     if (slackview) {
-        slackview.configure('TObfd0ab2772a04cb68098c8deca374878')
+        const token = 'TObfd0ab2772a04cb68098c8deca374878'
+        slackview.configure(token, {logLevel: 'debug'})
         slackview.render(document.getElementById('chat-slackview'))
     }
     else {
