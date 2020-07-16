3cd3494f1d5382d0c75a5afa7ebc55453277bf45
Carter Rabasa <carter.rabasa@gmail.com>
2020-07-16 14:51:30 -0700

    batch codes into groups of 25

--- src/http/post-codes/index.js
+++ src/http/post-codes/index.js
@@ -12,11 +12,22 @@ async function auth(req) {
 /** write to begin/data */
 async function upload(req) {
   let codes = req.body
-  for (let code of codes) {
-    await data.set({
-      table: 'codes',
-      code
-    })
+
+  // break-up our list of codes into groups of 25 (Begin data limit on batch sets)
+  let size = 25
+  var grouped = []
+  for(var i = 0; i < codes.length; i += size) {
+    grouped.push(
+      codes.slice(i, i+size).map(c => ({
+        table: 'codes',
+        key: c
+      }))
+    )
+  }
+  console.log(grouped)
+
+  for (let group of grouped) {
+    await data.set(group)
   }
 
   return {
