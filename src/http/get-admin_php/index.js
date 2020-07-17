67c77677ac77e0b4322a8a979eaa64925c0b1bbf
Carter Rabasa <carter.rabasa@gmail.com>
2020-07-17 15:26:16 -0700

    can't leave any props out, or they won't be written

--- src/http/get-admin_php/index.js
+++ src/http/get-admin_php/index.js
@@ -40,6 +40,7 @@ function ticket(t) {
       <summary>${ t.key } ${ t.ticket } ${ t.fullName } ${ t.conference === 'Y' ? '[Conf]' : '' } ${ t.hoodie === 'Y' ? '[Hoodie]' : '' }</summary>
       <form action=/ticket method=post>
         <input type=hidden name=key value="${ t.key }">
+        <input type=text name=ticket placeholder="Ticket Type" value="${ t.ticket || '' }">
         <input type=text name=fullName placeholder="Full Name" value="${ t.fullName || '' }">
         <input type=text name=conference placeholder="Conference (Y/N)" value="${ t.conference || '' }">
         <input type=text name=hoodie placeholder="Hoodie (Y/N)" value="${ t.hoodie || '' }">
