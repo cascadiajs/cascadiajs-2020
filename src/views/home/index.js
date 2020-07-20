f4354da16585467d50dc25efa18fb7f910daaa99
Carter Rabasa <carter.rabasa@gmail.com>
2020-07-20 14:37:20 -0700

    better goodie box info

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -7,12 +7,16 @@ module.exports = async function Index({ ticket }) {
             <div id=page-body class=narrow>
             <h2>Ticket Info</h2>
             <p><b>Type:</b> ${ ticket.ticket }</p>
-            ${ ticket.hoodie === 'Y' && ticket.code ? /*html*/`
-            <h2>Goodie Box Redemption</h2>
-            <p>You must redeem your Goodie Box by July 24 if you want to recieve it prior to the conference!</p>
-            <form action="https://stores.kotisdesign.com/cascadiajs2020/${ ticket.code }">
-                <button>Redeem Goodie Box</button>
-            </form>
+            ${ ticket.hoodie === 'Y' ? /*html*/`
+            <h2>Goodie Box</h2>
+                ${ ticket.code ? /*html*/`
+                <p>You must redeem your Goodie Box by July 24 if you want to recieve it prior to the conference!</p>
+                <form action="https://stores.kotisdesign.com/cascadiajs2020/${ ticket.code }">
+                    <button>Redeem Goodie Box</button>
+                </form>`
+                : /*html*/`
+                <p>We have your information and your Goodie Box will be delivered to the address provided prior to Sept 1.</p>
+                ` }
             ` : '' }
             <h2>Reset Session</h2>
             <p>If you need to use a different Ticket Reference, just reset the session and start over.
