8cd7c2c4dd6bd731da67d96f0dc000c01643d2a1
Carter Rabasa <carter.rabasa@gmail.com>
2020-08-10 20:32:44 -0700

    new sponsor logo layout

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -72,33 +72,37 @@ let Template = function(props) {
             <div>
                 <h2>Sponsors</h2>
                 <div id="sponsor-container">
-                    <section id="sponsors-platinum">
-                        <a href="https://developer.salesforce.com"><img src="/images/sponsors/sfdc.svg" alt="Salesforce.com Engineering logo"/></a>
-                    </section>
-                    <section id="sponsors-gold">
-                        <a href="https://launchdarkly.com"><img src="/images/sponsors/ld.svg" alt="Launch Darkly logo"/></a>
-                        <a href="https://heroku.com"><img src="/images/sponsors/heroku.svg" alt="Heroku logo"/></a>    
-                        <a href="https://auth0.com"><img src="/images/sponsors/auth0.svg" alt="Auth0 logo"/></a>
-                        <a href="https://circleci.com/"><img src="/images/sponsors/circle-ci.png" alt="Circle CI logo"/></a>
-                        <a href="https://twilio.com"><img src="/images/sponsors/twilio.svg" alt="Twilio logo"/></a>
-                        <a href="https://saucelabs.com"><img src="/images/sponsors/saucelabs.svg" alt="Twilio logo"/></a>
-                        <a href="https://tagboard.com"><img src="/images/sponsors/tagboard.png" alt="Tagboard logo"/></a>
-                    </section>
-                    <section id="sponsors-silver">
-                        <a href="https://fizbuz.com"><img src="/images/sponsors/fizbuz.svg" alt="Fizbuz logo"/></a>
-                        <a href="http://begin.com/"><img src="/images/sponsors/begin.svg" alt="Begin logo"/></a>
-                        <a href="https://mozilla.com"><img src="/images/sponsors/mozilla.svg" alt="Mozilla logo"/></a>
-                        <a href="https://developer.microsoft.com/en-us/advocates/"><img src="/images/sponsors/microsoft.svg" alt="Microsoft logo"/></a>
-                        <a href="/kotis-design"><img src="/images/sponsors/kotis-design-logo-black.svg" alt="Kotis logo"/></a>
-                    </section>
+                    <div id="sponsors-left">
+                        <section id="sponsors-platinum">
+                            <a href="https://developer.salesforce.com"><img src="/images/sponsors/sfdc.svg" alt="Salesforce.com Engineering logo"/></a>
+                        </section>
+                        <section id="sponsors-silver">
+                            <a href="https://fizbuz.com"><img src="/images/sponsors/fizbuz.svg" alt="Fizbuz logo"/></a>
+                            <a href="http://begin.com/"><img src="/images/sponsors/begin.svg" alt="Begin logo"/></a>
+                            <a href="https://mozilla.com"><img src="/images/sponsors/mozilla.svg" alt="Mozilla logo"/></a>
+                            <a href="https://developer.microsoft.com/en-us/advocates/"><img src="/images/sponsors/microsoft.svg" alt="Microsoft logo"/></a>
+                            <a href="/kotis-design"><img src="/images/sponsors/kotis-design-logo-black.svg" alt="Kotis logo"/></a>
+                        </section>
+                        <section id="sponsors-community">    
+                            <a href="http://seattlejs.com/"><img src="/images/sponsors/seattlejs.png" alt="SeattleJS logo"/></a>
+                            <a href="https://www.meetup.com/ReactJS-Vancouver-Meetup/"><img src="/images/sponsors/reactvancouver.svg" alt="React Vancouver logo"/></a>
+                            <a href="https://www.meetup.com/SeattleVueJS/"><img src="/images/sponsors/sea-vuejs.jpg" alt="Seattle Vue.js logo"/></a>
+                            <a href="http://pdxnode.org/"><img src="/images/sponsors/pdx-node.svg" alt="PDXNode logo"/></a>
+                            <a href="https://www.meetup.com/Seattle-Node-js/"><img src="/images/sponsors/seattle-node.png" alt="Seattle Node logo"/></a>
+                        </section>
+                    </div>
+                    <div id="sponsors-right">
+                        <section id="sponsors-gold">
+                            <a href="https://launchdarkly.com"><img src="/images/sponsors/ld.svg" alt="Launch Darkly logo"/></a>
+                            <a href="https://heroku.com"><img src="/images/sponsors/heroku.svg" alt="Heroku logo"/></a>    
+                            <a href="https://auth0.com"><img src="/images/sponsors/auth0.svg" alt="Auth0 logo"/></a>
+                            <a href="https://circleci.com/"><img src="/images/sponsors/circle-ci.png" alt="Circle CI logo"/></a>
+                            <a href="https://twilio.com"><img src="/images/sponsors/twilio.svg" alt="Twilio logo"/></a>
+                            <a href="https://saucelabs.com"><img src="/images/sponsors/saucelabs.svg" alt="Twilio logo"/></a>
+                            <a href="https://tagboard.com"><img src="/images/sponsors/tagboard.png" alt="Tagboard logo"/></a>
+                        </section>
+                    </div>
                 </div>
-                <section id="sponsors-community">    
-                    <a href="http://seattlejs.com/"><img src="/images/sponsors/seattlejs.png" alt="SeattleJS logo"/></a>
-                    <a href="https://www.meetup.com/ReactJS-Vancouver-Meetup/"><img src="/images/sponsors/reactvancouver.svg" alt="React Vancouver logo"/></a>
-                    <a href="https://www.meetup.com/SeattleVueJS/"><img src="/images/sponsors/sea-vuejs.jpg" alt="Seattle Vue.js logo"/></a>
-                    <a href="http://pdxnode.org/"><img src="/images/sponsors/pdx-node.svg" alt="PDXNode logo"/></a>
-                    <a href="https://www.meetup.com/Seattle-Node-js/"><img src="/images/sponsors/seattle-node.png" alt="Seattle Node logo"/></a>
-                </section>
                 <div class="cta"><a href="/sponsor">Sponsor Info</a></div>
             </div>
         </div>
