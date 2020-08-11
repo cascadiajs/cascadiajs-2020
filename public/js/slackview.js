1751da4e51b0fc30376cf5331a396c7cf585bac9
Carter Rabasa <carter.rabasa@gmail.com>
2020-08-11 14:16:45 -0700

    august meetup updates

--- public/js/slackview.js
+++ public/js/slackview.js
@@ -2,8 +2,8 @@
 window.onload = function() {
     // Attach builtin widget to your page
     if (slackview) {
-        const token = 'TObfd0ab2772a04cb68098c8deca374878'
-        //const token = 'TOed3c03eb7d39493585f2a35b580b5d20'
+        //const token = 'TObfd0ab2772a04cb68098c8deca374878'
+        const token = 'TOed3c03eb7d39493585f2a35b580b5d20'
         slackview.configure(token, {logLevel: 'debug'})
         slackview.render(document.getElementById('chat-slackview'))
     }
