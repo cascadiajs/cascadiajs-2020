1e8ac00e9f2c5581484a28f27e0d56dfefaed788
Carter Rabasa <carter.rabasa@gmail.com>
2020-08-26 20:30:48 -0700

    adding /home secret links

--- src/views/home/form.js
+++ src/views/home/form.js
@@ -3,10 +3,11 @@ let Layout = require('../layout')
 module.exports = async function({ message }) {
     let content = /*html*/`
         <div id=page>
-            <div id=page-title><div><h1>Validate Ticket</h1></div></div>
-            <div id=page-body class=narrow>
+            <div class=page-title><div><h1>Validate Ticket</h1></div></div>
+            <div class=page-body class=narrow>
             ${ message ? `<div class="highlight error">${ message }</div>` : '' }
-                <p>Please enter your Ticket Reference below. You can find this in an email that was sent to you with the subject "Your CascadiaJS 2020 Ticket".</p>
+                <p>Please enter your Ticket Reference below.<p>
+                <p><em>Tip: You can find this in an email that was sent to you with the subject "Your CascadiaJS 2020 Ticket".</em></p>
                 <form action=/home method=post>
                     <input style="width:100%;margin-bottom:8px" type=text placeholder="Ticket Reference (i.e. ABCD-1)" name=ticketRef>
                     <button>Submit</button>
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -5,26 +5,37 @@ module.exports = async function Index({ ticket }) {
         <div id=page>
             <div class=page-title><div><h1>Hello${ ticket.fullName ? ', ' + ticket.fullName : '' }!</h1></div></div>
             <div class=page-body class=narrow>
+            <span class="highlight warning">PLEASE READ: IMPORTANT NOTE FOR ALL ATTENDEES</span>
+            <p>The URLs below are <em>private</em> and only meant for use by registered attendees of CascadiaJS 2020.</p>
+            <p><b>Sharing these links with anyone who is not a registered attendee will be considered a <a href="/coc">Code of Conduct</a> violation.</b></p>
+            <p>If you see anyone sharing or distributing these links, please let an organizer know. Help us keep CascadiaJS a <span class="highlight info">safe place</span> for everyone.</p>
             <h2>Ticket Info</h2>
+            ${ ticket.hoodie && ticket.code ? '<p><em>Note: Deadline to redeem Goodie Box is Wed, 9/2</em></p>' : ''}
             <ul>
                 <li><b>Ticket Type:</b> ${ ticket.ticket }</li>
                 <li><b>Conference:</b> ${ ticket.conference == 'Y' ? "Yes" : "No" }</li>
-                <li><b>Goodie Box:</b> ${ ticket.hoodie === 'Y' ? "Yes" : "No" }</li>
+                <li><b>Goodie Box:</b> ${ ticket.hoodie === 'Y' 
+                    ? ticket.code
+                        ? `<a href="https://stores.kotisdesign.com/cascadiajs2020/${ ticket.code }">Redeem Goodie Box</a>` 
+                        : "Yes"
+                    : "No" }
+                </li>
             </ul>
-            ${ ticket.hoodie === 'Y' ? /*html*/`
-            <h2>Goodie Box</h2>
-                ${ ticket.code ? /*html*/`
-                <div class="highlight warning">You must redeem this by Friday, August 28</div>
-                <div class="cta"><a href="https://stores.kotisdesign.com/cascadiajs2020/${ ticket.code }">Redeem Goodie Box</a></div>`
-                : /*html*/`
-                <p>We have your information and your Goodie Box will be delivered to the address provided during registration.</p>
-                ` }
-            ` : '' }
             ${ ticket.conference === 'Y' ? /*html*/`
             <h2>Career Night (8/31)</h2>
-            <div class="cta"><a href="${ process.env.CAREER_NIGHT_URL }">RSVP</a></div>
-            <h2>Conference Live Stream (9/1 &amp; 9/2)</h2>
-            <div class="cta"><a href="/live">Live Stream</a></div>
+            <ul>
+                <li><span class="cta"><a href="${ process.env.CAREER_NIGHT_URL }" target="_hallway">RSVP</a></span></li>
+                <li><span class="cta"><a href="${ process.env.REMO_CAREER_URL }">Attend / Remo (8/31)</a></span></li>
+            </ul>
+            <h2>Conference (9/1 &amp; 9/2)</h2>
+            <ul>
+                <!--li><span class="cta"><a href="/live" target="_live">Live Stream Track</a></span></li-->
+                <li><span class="cta"><a href="${ process.env.RAMBLY_URL }" target="_rambly">Hallway Track / Rambly</a></span></li>
+                <li><span class="cta"><a href="${ process.env.REMO_DAYONE_URL }" target="_hallway">Hallway Track / Remo (Sept 1)</a></span></li>
+                <li><span class="cta"><a href="${ process.env.REMO_DAYTWO_URL }" target="_hallway">Hallway Track / Remo (Sept 2)</a></span></li>
+                <li><span class="cta"><a href="${ process.env.KARAOKE_URL_1 }" target="_karaoke">Karaoke Room #1 / Zoom (Sept 2)</a></span></li>
+                <li><span class="cta"><a href="${ process.env.KARAOKE_URL_1 }" target="_karaoke">Karaoke Room #2 / Zoom (Sept 2)</a></span></li>
+            </ul>
             ` : '' }
             <h2>Reset Session</h2>
             <p>If you need to use a different Ticket Reference, just reset the session and start over.
