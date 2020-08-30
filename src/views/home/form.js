097bbb353eed91d1d270505ee0459aa9135bf7d6
Carter Rabasa <carter.rabasa@gmail.com>
2020-08-30 16:04:39 -0700

    nav & log-in tweaks

--- src/views/home/form.js
+++ src/views/home/form.js
@@ -3,11 +3,14 @@ let Layout = require('../layout')
 module.exports = async function({ message }) {
     let content = /*html*/`
         <div id=page>
-            <div class=page-title><div><h1>Validate Ticket</h1></div></div>
+            <div class=page-title><div><h1>Log-in to Your Conference Dashboard</h1></div></div>
             <div class=page-body class=narrow>
             ${ message ? `<div class="highlight error">${ message }</div>` : '' }
-                <p>Please enter your Ticket Reference below.<p>
-                <p><em>Tip: You can find this in an email that was sent to you with the subject "Your CascadiaJS 2020 Ticket".</em></p>
+                <h2>Welcome to the show!</h2>
+                <p>Everyone who bought a ticket to CascadiaJS 2020 was assigned a unique ticket code. This ticket code
+                is what you will use to log-in to your Conference Dashboard.</p>
+                <p>You can find your ticket code in an email that was sent to you with the subject "Your CascadiaJS 2020 Ticket". See below
+                for a screenshot that will help you located the ticket code.</p>
                 <form action=/home method=post>
                     <input style="width:100%;margin-bottom:8px" type=text placeholder="Ticket Reference (i.e. ABCD-1)" name=ticketRef>
                     <button>Submit</button>
