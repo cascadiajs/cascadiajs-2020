1dd25acebea671168af486be03289be238d9ac9b
Carter Rabasa <carter.rabasa@gmail.com>
2020-08-31 14:38:58 -0700

    TwilioQuest leaderboard

--- src/views/layout/live.js
+++ src/views/layout/live.js
@@ -20,6 +20,9 @@ module.exports = function Layout ({title, content, socialUrl = 'https://2020.cas
             <div>
               <span class="cta"><a href="/handbook" target="_handbook">Handbook</a></span>
             </div>
+            <div style="margin-left:8px;">
+              <span class="cta"><a href="https://www.twilio.com/quest/events/CascadiaJS/players" target="_twilioquest">TwilioQuest Leaderboard</a></span>
+            </div>
             <div id="settings-label-slackview" class="label">Slack View</div>
             <div id="settings-switch-slackview">
               <label class="switch">
