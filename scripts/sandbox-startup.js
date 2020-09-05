4528f927ec84e44a5ca8f3b8dc018e1b9bfe35b1
Carter Rabasa <carter.rabasa@gmail.com>
2020-09-04 19:11:22 -0700

    no logging

--- scripts/sandbox-startup.js
+++ scripts/sandbox-startup.js
@@ -4,7 +4,7 @@ let speakers = require('../data/testing/speakers.json')
 module.exports = async function populate() {
   try {
     let loaded = await data.set(speakers)
-    console.log('Added speakers: \n', loaded)
+    //console.log('Added speakers: \n', loaded)
   } catch(err) {
     console.error('ERROR: ', err)
   }
