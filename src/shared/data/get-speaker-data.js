dcb4f12ba84da6557e3d483e01626ede82ba431f
Carter Rabasa <carter.rabasa@gmail.com>
2020-09-04 17:02:06 -0700

    debugging

--- src/shared/data/get-speaker-data.js
+++ src/shared/data/get-speaker-data.js
@@ -1,6 +1,7 @@
 let speakers = require('./speakers.json')
-
+console.log("outside function: ", speakers)
 module.exports = async function GetSpeakerData (req) {  
+  console.log("inside function: ", speakers)
   // scrub info for non-revealed speakers (only include reveal & pixelated props)
   speakers = speakers.map(s => ((new Date(s.reveal)).getTime() <= Date.now() ? s : { reveal: s.reveal, pixelated: s.pixelated, topics: s.topics }))
   // Collect all the speakers topics into an array with no duplicate topics
